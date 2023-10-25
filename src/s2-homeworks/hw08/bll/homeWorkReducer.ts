c8e527d2fa4ca9a05af46c06de624366527eda98
Сергей <sergbfmv@mail.ru>
2023-10-25 11:55:27 +0300

    8th homework done

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,22 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
 
-            return state // need to fix
+            if (action.payload === 'up') {
+                return [...state].sort((a, b) => a.name.localeCompare(b.name)) // need to fix
+            } else if (action.payload === 'down') {
+                return [...state].sort((a, b) => b.name.localeCompare(a.name))
+            } else {
+                return state
+            }
+
         }
         case 'check': {
 
-            return state // need to fix
+            return state.filter(a => a.age >= action.payload) // need to fix
         }
         default:
             return state
