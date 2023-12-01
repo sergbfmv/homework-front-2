3fcd9008f726948b1c85e38e244ff4d794b44a63
sergbfmv <sergbfmv@mail.ru>
2023-12-01 14:28:46 +0300

    HW 12 is done

--- src/s2-homeworks/hw12/HW12.tsx
+++ src/s2-homeworks/hw12/HW12.tsx
@@ -3,12 +3,12 @@ import s from './HW12.module.css'
 import s2 from '../../s1-main/App.module.css'
 import SuperSelect from '../hw07/common/c5-SuperSelect/SuperSelect'
 import {useDispatch, useSelector} from 'react-redux'
-import {changeThemeId} from './bll/themeReducer'
+import {changeThemeId, RootType, StateType} from './bll/themeReducer'
 
 /*
-* 1 - в файле themeReducer.ts написать нужные типы вместо any, дописать редьюсер
-* 2 - получить themeId из редакса
-* 3 - дописать тип и логику функции change
+* 1 - в файле themeReducer.ts написать нужные типы вместо any, дописать редьюсер ++
+* 2 - получить themeId из редакса ++
+* 3 - дописать тип и логику функции change ++
 * 4 - передать пропсы в SuperSelect
 * */
 
@@ -20,10 +20,12 @@ const themes = [
 
 const HW12 = () => {
     // взять ид темы из редакса
-    const themeId = 1
+    const themeId = useSelector((state: RootType) => state.theme.themeId);
+    const dispatch = useDispatch()
+    console.log(themeId)
 
-    const change = (id: any) => { // дописать функцию
-
+    const change = (id: number) => { // дописать функцию
+        dispatch(changeThemeId(id))
     }
 
     useEffect(() => {
@@ -40,6 +42,8 @@ const HW12 = () => {
                 <SuperSelect
                     id={'hw12-select-theme'}
                     className={s.select}
+                    onChangeOption={(id) => change(id)}
+                    options={themes}
                     // сделать переключение тем
 
                 />
--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,38 @@
-const initState = {
+import { createStore, combineReducers } from 'redux';
+
+const initState: StateType = {
     themeId: 1,
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+export const themeReducer = (state = initState, action: Actionstype): StateType => { // fix any
     switch (action.type) {
         // дописать
-
+        case "SET_THEME_ID":
+            return {...state, themeId: action.id}
         default:
             return state
     }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+export const changeThemeId = (id: number) => ({ type: 'SET_THEME_ID', id }) // fix any
+
+
+export type StateType = {
+    themeId: number
+}
+type ChangeThemeIdType = ReturnType<typeof changeThemeId>
+type Actionstype = ChangeThemeIdType
+
+
+
+
+
+
+const rootReducer = combineReducers({
+    // ...other reducers
+    theme: themeReducer,
+});
+
+export type RootType = ReturnType<typeof rootReducer>
+
+export const store = createStore(rootReducer);
